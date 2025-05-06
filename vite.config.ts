29cc07864b2cb2cd4b6ed4342a4b7ca0c73c2038
Daniel Asfaw <asfawdaniel779@gmail.com>
2025-05-06 03:30:11 +0300

    add: read me

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,25 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//   },
+//   server: {
+//     middlewareMode: true,
+//   },
+// });
